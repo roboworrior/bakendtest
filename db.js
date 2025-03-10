3025493166cdcdc2a9aaf0ac8b97f155a1b57ae2
Akshay sharma <akshaysharmaak57@gmail.com>
2025-03-10 16:54:50 +0530

    'init'

--- db.js
+++ db.js
@@ -1,14 +1,28 @@
-const { Pool } = require('pg');  
+require('dotenv').config();
+const { Pool } = require('pg');
 
-// Replace this with your Neon connection string  
+// Replace this with your Neon connection string
 const connectionString = process.env.DATABASE_URL;
 
-// Create a new pool  
-const pool = new Pool({ 
-  connectionString,  
-  ssl: {  
-    rejectUnauthorized: false  
-  }  
-});  
+// Create a new pool
+const pool = new Pool({
+  connectionString,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+pool.connect((err, client, release) => {
+  if (err) {
+    return console.error('Error acquiring client', err.stack);
+  }
+  client.query('SELECT NOW()', (err, result) => {
+    release();
+    if (err) {
+      return console.error('Error executing query', err.stack);
+    }
+    console.log('Connected to the database:', result.rows);
+  });
+});
 
 module.exports = pool;  

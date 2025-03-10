fddd47a8cb6903a9bbdafc7bc863e1c75ef6e159
Akshay sharma <akshaysharmaak57@gmail.com>
2025-03-10 13:27:58 +0530

    '.env'

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const { Pool } = require('pg');  
 
 // Replace this with your Neon connection string  
-const connectionString = 'postgresql://neondb_owner:npg_35vQNnJsZjcR@ep-cool-heart-a8dg10ct-pooler.eastus2.azure.neon.tech/neondb?sslmode=require';
+const connectionString = process.env.DATABASE_URL;
 
 // Create a new pool  
-const pool = new Pool({  
+const pool = new Pool({ 
   connectionString,  
   ssl: {  
     rejectUnauthorized: false  

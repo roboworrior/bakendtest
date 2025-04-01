f651850469f2c07a78eb275334c7bff74b8deed8
Akshay sharma <akshaysharmaak57@gmail.com>
2025-04-01 06:44:11 +0530

    data

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const app = express();
 app.use(express.json());  
 app.use(cors());  
 // Example route to test the database connection  
+
 app.get('/test', async (req, res) => {  
   try {  
     const result = await pool.query('SELECT * FROM userinfo');  
@@ -19,6 +20,18 @@ app.get('/test', async (req, res) => {
   }  
 });  
 
+app.get('/data', async (req, res) => {  
+  try {  
+    const result = await pool.query('SELECT * FROM data');  
+    res.json(result.rows);  
+
+  } catch (err) {  
+    console.error(err);  
+    res.status(500).json({ message: 'Database error' });  
+  }  
+});  
+
+
 
 app.post('/api/submit', async (req, res) => {  
     try {  
@@ -26,6 +39,42 @@ app.post('/api/submit', async (req, res) => {
         console.log("this is the data",cartItems);
         
         const result = await pool.query('INSERT INTO userinfo(name,phone_number,cart) VALUES ($1, $2 ,$3) RETURNING *',[name, phone_number ,JSON.stringify(cartItems)]);  
+
+        res.status(201).json(result.rows[0]);  
+    } 
+    
+    catch (error) {  
+        console.error('Error saving data:', error);  
+        res.status(500).json({ message: 'Error saving data' });  
+    }  
+}); 
+
+app.post('/api/login', async (req, res) => {  
+    try {  
+        const { email,password} = req.body;  
+       
+        // const result = await pool.query('INSERT INTO logindata(email,password) VALUES ($1, $2) RETURNING *',[email,password]);  
+
+        const user = await pool.query('SELECT * FROM logindata WHERE email = $1', [email]);
+        
+        if (user.rows.length === 0) {
+            return res.status(401).json({ message: 'Invalid email or password' });
+        }
+        alert("dsfdsf");
+        
+    } 
+    
+    catch (error) {  
+        console.error('Error saving data:', error);  
+        res.status(500).json({ message: 'Error saving data' });  
+    }  
+}); 
+
+app.post('/api/registor', async (req, res) => {  
+    try {  
+        const { username,password,email} = req.body;  
+       
+        const result = await pool.query('INSERT INTO logindata(username,password,email) VALUES ($1, $2,$3) RETURNING *',[username,password,email]);  
 
         res.status(201).json(result.rows[0]);  
     } 

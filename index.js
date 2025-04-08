5a30274e0d28f4fc96b3e2b1feeba0366ff89f99
Akshay sharma <akshaysharmaak57@gmail.com>
2025-04-08 15:06:06 +0530

    sdf

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const pool = require('./db');  
 const cors = require('cors');  
 
-
+const bcrypt = require('bcrypt');
 const app = express(); 
 
 // Middleware  

3db9046d64247e3c7916bb69c2ea4ae823169aae
Akshay sharma <akshaysharmaak57@gmail.com>
2025-09-14 16:56:02 +0530

    vercel upadad fule

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 const express = require('express');  
-const pool = require('./db');  
+const pool = require('../bakendtest/db');  
 const cors = require('cors');  
 
 const bcrypt = require('bcrypt');

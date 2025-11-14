fe254683b83825237da2873fe48c22008f90d614
roboworrior <34987549+roboworrior@users.noreply.github.com>
2025-11-14 15:12:37 +0530

    dfdfd

--- api/index.js
+++ api/index.js
@@ -176,7 +176,7 @@ app.post('/api/myorder', async (req, res) => {
         const { email } = req.body;
 
         if (!email) {
-            return res.status(400).json({ message: 'Email is required' });
+            return res.status(400).json({ message: 'dEmail is required',email });
         }
 
         const result = await pool.query(

d78c04afc78739e328c08c8bf8ab7fc2adbfb4d1
roboworrior <34987549+roboworrior@users.noreply.github.com>
2025-11-16 01:15:58 +0530

    this adin

--- api/index.js
+++ api/index.js
@@ -184,9 +184,9 @@ app.post('/api/myorder', async (req, res) => {
             [email]
         );
         
-        // if(result.rows.length == 0){
-        //     return res.status(500).json({ message: 'No orders found',data:[] });
-        // }
+        if(result.rows.length == 0){
+            return res.status(401).json({ message: 'No orders found',data:[] });
+        }
 
         // return res.status(200).json(result.rows);
         return res.status(200).json({ message: 'Server Pass' });

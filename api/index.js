632e60ef6051dce5cb8e47a6266694ab2b088087
roboworrior <34987549+roboworrior@users.noreply.github.com>
2025-11-16 01:03:16 +0530

    admin hwll

--- api/index.js
+++ api/index.js
@@ -184,10 +184,10 @@ app.post('/api/myorder', async (req, res) => {
             [email]
         );
         
-        if(result.rows.length < 1){
+        if(result.rows.length==0){
             return res.status(401).json({ message: 'No orders found' });
-
         }
+
         return res.status(200).json(result.rows);
 
     }

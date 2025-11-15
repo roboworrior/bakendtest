57f135695fca571ebc1a8612206ffc03a5645b05
roboworrior <34987549+roboworrior@users.noreply.github.com>
2025-11-16 02:32:44 +0530

    submit

--- api/index.js
+++ api/index.js
@@ -159,7 +159,7 @@ app.post('/api/submit', async (req, res) => {
     try {
         const { name, phone_number, email, cartItems } = req.body;
 
-          if (!email || !name || !phone_number) {
+          if (!email || !name || !phone_number || !cartItems) {
             return res.status(401).json({ message: 'Please fill in all the required fields.'});
         }
        

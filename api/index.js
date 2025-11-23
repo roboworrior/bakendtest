af4290b6fbdb781aaf9ab9353a3da91c0bdad5f9
roboworrior <34987549+roboworrior@users.noreply.github.com>
2025-11-23 20:40:16 +0530

    commit 1m

--- api/index.js
+++ api/index.js
@@ -229,7 +229,7 @@ app.post('/api/login', async (req, res) => {
                 role: role
             },
             process.env.JWT_SECRET,
-            { expiresIn: "30m" }
+            { expiresIn: "1m" }
         );
 
         res.status(200).json({
